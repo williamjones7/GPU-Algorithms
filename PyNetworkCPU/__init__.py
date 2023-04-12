0d434c6f31abbcaa28c3f1f586ca83ad44ba6b13
Do-J <s1951467@ed.ac.uk>
2023-04-12 12:31:42 +0100

    Oringinal PyNetwork package for testing

---
+++ PyNetworkCPU/__init__.py
@@ -0,0 +1,19 @@
+# Activation and Cost Functions
+from PyNetworkCPU.functions import get_activation_function
+from PyNetworkCPU.functions import get_error_function
+from PyNetworkCPU.functions import get_metric_function
+
+# Sequential Class
+from PyNetworkCPU.Sequential import Sequential
+
+# Network Layers
+import PyNetworkCPU.layers as layers
+
+# Optimisers
+import PyNetworkCPU.optimizers as optimizers
+
+# Exceptions
+from PyNetworkCPU import exceptions
+
+# Validation Checks
+from PyNetworkCPU import validation
